4d5914d49da64ca2ba09eecdb663796698baf17a
bau-steinchen <mikko.ristein@gmail.com>
2023-01-10 13:04:44 +0000

    adding base file for update script

---
+++ updateServer.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# This script will be used to update te server files to run the given version
+# This program will also create a backup of the most urgent files but i may bring unforseen situation make sure you do not loose urgen files
+# param (1) - will be the path to the server-files.zip
+# param (2) - will be the path to the current server files
+
+echo "not yet implemented"
+exit(1)
